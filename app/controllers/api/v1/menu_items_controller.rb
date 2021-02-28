338733b267d88a94f6bcf92e0968e7b7d618af89
Lionnel Patrick DOOKO <patrick.lionnel@finetechgroupe.com>
2021-02-28 12:15:13 +0000

    add main routes and controllers

---
+++ app/controllers/api/v1/menu_items_controller.rb
@@ -0,0 +1,34 @@
+module Api
+    module V1
+        class MenuItemsController < ApplicationController
+            before_action :set_menu_item, only: [:show]
+            def index 
+                @menu_items = MenuItem.all
+
+                render json: @menu_items.to_json(:include => {
+                    :dish => {:except => [:created_at, :updated_at]},
+                    :menu => {:only => [:date]}
+                  }, :except => [:updated_at])
+            end
+
+            def show
+                render json: @menu_item.to_json(:include => {
+                    :dish => {:except => [:created_at, :updated_at]},
+                    :menu => {:only => [:date]}
+                  }, 
+                  :except => [:updated_at]
+                )
+            end
+
+            private 
+            
+            def set_menu_item
+                @menu_item = MenuItem.find(params[:id])
+            end
+
+            def menu_item_params
+                params.require(:menu_item).permit(:dish_id, :menu_id)
+            end
+        end
+    end
+end
---
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -0,0 +1,30 @@
+module Api
+    module V1
+        class SubscriptionsController < ApplicationController
+            before_action :set_subcription, only: [:show]
+            def index 
+                @subcriptions = Subscription.all
+
+                render json: @subcriptions.to_json(:include => {
+                    :user => {:only => [:email]}
+                  }, :except => [:updated_at])
+            end
+
+            def show
+                render json: @subcription.to_json(:include => {
+                    :user => {:only => [:email]}
+                  }, :except => [:updated_at])
+            end
+
+            private 
+            
+            def set_subcription
+                @subcription = Subscription.find(params[:id])
+            end
+
+            def subcription_params
+                params.require(:subcription).permit(:user_id, :status, :period_start, :period_end)
+            end
+        end
+    end
+end

d2b0a8e96445f62d6f255af8cb5a4738f6818cd4
Lionnel Patrick DOOKO <patrick.lionnel@finetechgroupe.com>
2021-03-08 15:20:55 +0000

    change api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
     def index 
         res = {
             "name": "Finelunch api",
-            "version": "1.0.0",
+            "version": "1.0.1",
             "description": "Simple rails api for finelunch app",
             "repository": {
                 "type": "git",

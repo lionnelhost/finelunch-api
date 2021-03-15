f7fa0a1fb0e1339a2090fda218b81eff39cdf234
Lionnel Patrick DOOKO <patrick.lionnel@finetechgroupe.com>
2021-03-15 10:49:59 +0000

    list all users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,29 +1,29 @@
 module Api
     module V1
         class UsersController < ApplicationController
-            before_action :set_profile, only: [:show]
+            before_action :set_user, only: [:show]
             def index 
-                @users = Profile.all
+                @users = User.includes(:profile).all
 
                 render json: @users.to_json(:include => {
-                    :user => {
-                        :only => [:email]
+                    :profile => {
+                        :except => [:created_at, :updated_at]
                     },
-                  }, :except => [:updated_at])
+                  }, :except => [:created_at, :updated_at])
             end
 
             def show
                 render json: @user.to_json(:include => {
-                    :user => {
-                        :only => [:email]
+                    :profile => {
+                        :except => [:created_at, :updated_at]
                     },
-                  }, :except => [:updated_at])
+                  }, :except => [:created_at, :updated_at])
             end
 
             private 
             
-            def set_profile
-                @user = Profile.find(params[:id])
+            def set_user
+                @user = User.find(params[:id])
             end
 
         end

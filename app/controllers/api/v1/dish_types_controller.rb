ac7d1558693f46b68b8fc5df4c4abf4755c04b78
Lionnel Patrick DOOKO <patrick.lionnel@finetechgroupe.com>
2021-03-16 16:32:57 +0000

    add errors handling

--- app/controllers/api/v1/dish_types_controller.rb
+++ app/controllers/api/v1/dish_types_controller.rb
@@ -15,45 +15,30 @@ module Api
             def create
                 @dish_type = DishType.new(dish_type_params)
 
-                if @dish_type.save 
+                if @dish_type.save! 
                     render json: {
                         code: :created,
                         message: "dish type create successfully"
                     }, status: :created
-                else
-                    render json:  {
-                        code: 500,
-                        message: @dish_type.errors.full_messages
-                    }, status: :unprocessable_entity
                 end
             end
 
             def update
 
-                if @dish_type.update(dish_type_params)
+                if @dish_type.update!(dish_type_params)
                     render json: {
                         code: :updated,
                         message: "dish type updated successfully"
                     }, status: :created
-                else
-                    render json:  {
-                        code: 500,
-                        message: @dish_type.errors.full_messages
-                    }, status: :unprocessable_entity
                 end
             end
 
             def destroy 
-                if @dish_type.destroy
+                if @dish_type.destroy!
                     render json: {
                         code: :deleted,
                         message: "dish type destroyed successfully"
                     }, status: :created
-                else
-                    render json:  {
-                        code: 500,
-                        message: @dish_type.errors.full_messages
-                    }, status: :unprocessable_entity
                 end
             end
             
--- app/models/dish_type.rb
+++ app/models/dish_type.rb
@@ -2,6 +2,8 @@ class DishType < ApplicationRecord
 
     has_many :dishes, dependent: :destroy
     
+    validates_uniqueness_of :name
+
     validates :name, presence: true, length: {maximum: 50, minimum: 2 }
-    validates :name, presence: true
+    validates :extra, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
